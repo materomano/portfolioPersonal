fb131e7b5a1d1b11a635209bdb368ba489712216
mateo romano <mate.romano83@gmail.com>
2024-07-22 09:06:00 -0300

    mateo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import { encabezado } from '../Estilo/header.module.css'; // Importa estilos locales de módulo CSS
-import { useLang } from '../components/LangContext';
+import { useLang } from '../components/LangContext'; // Asumiendo que tienes un contexto de idioma
 
 const Header = () => {
-  const { switchLang } = useLang();
-  
+  const { switchLang, lang } = useLang(); // Obtenemos switchLang y lang del contexto
+
+  // Función para renderizar los <li> dependiendo del idioma
+  const renderMenuItems = () => {
+    if (lang === 'es') {
+      // Renderiza los elementos en español
+      return (
+        <>
+          <li>
+            <a href="#portada">Inicio</a>
+          </li>
+          <li>
+            <a href="#sobre-mi">Sobre Mi</a>
+          </li>
+          <li>
+            <a href="#proyectos">Proyectos</a>
+          </li>
+          <li>
+            <a href="#estudios">Estudios</a>
+          </li>
+        </>
+      );
+    } else if (lang === 'en') {
+      // Renderiza los elementos en inglés
+      return (
+        <>
+          <li>
+            <a href="#portada">Home</a>
+          </li>
+          <li>
+            <a href="#sobre-mi">About Me</a>
+          </li>
+          <li>
+            <a href="#proyectos">Projects</a>
+          </li>
+          <li>
+            <a href="#estudios">Studies</a>
+          </li>
+        </>
+      );
+    }
+  };
+
   return (
     <div className={encabezado}>
       <ul>
-        <li>
-          <a href="#portada">Inicio</a>
-        </li>
-        <li>
-          <a href="#sobre-mi">Sobre Mi</a>
-        </li>
-        <li>
-          <a href="#proyectos">Proyectos</a>
-        </li>
-        <li>
-          <a href="#estudios">Estudios</a>
-        </li>
+        {renderMenuItems()} 
       </ul>
       <ul>
         <li>
