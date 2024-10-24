36353fc501fdb7fde886f9715dd241fbe64ed82e
mateo romano <mate.romano83@gmail.com>
2024-10-24 18:11:51 -0300

    ;;

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   
-    base: '/mateoromano.github.io/', // Ajusta esto según tu repositorio
+     base: '/portfolioPersonal/'
  
   
 })
